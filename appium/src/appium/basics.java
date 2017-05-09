e31c1e69bb6bbb69fd93c4b897cba686c824ca62
unknown <dung.tran@dungtranpc.infonam.com>
2017-05-09 17:33:55 +0700

    1st commit

---
+++ appium/src/appium/basics.java
@@ -0,0 +1,15 @@
+package appium;
+
+import java.net.MalformedURLException;
+
+import bsh.Capabilities;
+import io.appium.java_client.android.AndroidDriver;
+
+public class basics extends Base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		AndroidDriver driver=Capabilities();
+	}
+
+}
