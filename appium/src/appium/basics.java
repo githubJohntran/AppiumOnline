c5a3dbebd32d1299e43e2ea89a67522601457474
johntran <trandung080586@gmail.com>
2017-05-09 22:17:14 +0700

    section 5.25

--- appium/src/appium/basics.java
+++ appium/src/appium/basics.java
@@ -1,6 +1,9 @@
 package appium;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.rules.Timeout;
 
 import bsh.Capabilities;
 import io.appium.java_client.android.AndroidDriver;
@@ -10,6 +13,13 @@ public class basics extends Base{
 	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
 		AndroidDriver driver=Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//xpath
+		/*xpath syntax
+		 * //tagnam[@attribute='value']
+		 */
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
 	}
 
 }
